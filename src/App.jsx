99b4be0f2080ef9d88fed6a4e7b501cf464195ad
Francisco Santiago <44105867+MasterFSC@users.noreply.github.com>
2022-07-20 23:38:55 -0500

    creacion de login con Mobile First

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+export const App = () => 
+  (
+    <div className="container">
+      <form>
+        <h1>Iniciar sesión</h1>
+        <div>
+          <label>Email</label>
+          <input name="email" type="email"/>
+        </div>
+        <div>
+          <label>Contraseña</label>
+          <input name="password" type="password"/>
+        </div>
+        <div>
+          <button type="submit">Enviar</button>
+        </div>
+      </form>
+     
+    </div>
+  )
+
+
+export default App
