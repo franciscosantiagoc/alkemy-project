f7aa49aa650cc529d71be32492bafa98f4f1016c
Francisco Santiago <44105867+MasterFSC@users.noreply.github.com>
2022-07-22 15:08:25 -0500

    Agregando formik al login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,28 +1,50 @@
 import React, { useState } from 'react';
+import { useFormik } from 'formik';
 
 export const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if(!email || !password){
-      alert("Es indispensable que aprendas cosas interesantes que completes")
-    }else {
-      window.location = "https://cybermap.kaspersky.com";
-    }
+  const initialValues = {
+    email: '',
+    password: '',
   }
+
+  const validate = values => {
+    const errors = {};
+
+    if(!values.email){
+      errors.email = 'El email es requerido';
+    }
+    if(!values.password) {
+      errors.password = 'El password es requerido';
+    }
+
+    return errors;
+  }
+
+  const onSubmit = (event) => {
+    localStorage.setItem('logged', 'yes');
+  }
+
+  const formik = useFormik({
+    initialValues,
+    validate,
+    onSubmit,
+  })
+
   return (
     <div className="container">
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <h1>Iniciar sesión</h1>
         <div>
           <label>Email</label>
-          <input name="email" type="email" value={email} onChange={event => setEmail(event.currentTarget.value)}/>
+          <input name="email" type="email" value={formik.values.email} onChange={formik.handleChange}/>
+          {formik.errors.email && (<span>{formik.errors.email}</span>)}
         </div>
         <div>
           <label>Contraseña</label>
-          <input name="password" type="password" value={password} onChange={event => setPassword(event.currentTarget.value)}/>
+          <input name="password" type="password" value={formik.values.password} onChange={formik.handleChange}/>
+          {formik.errors.password && (<span>{formik.errors.password}</span>)}
         </div>
         <div>
           <button type="submit">Enviar</button>
